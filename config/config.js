9128514f57e2e7c35a2bbdc3ee1118e8d815e494
farislr <lrfaris24@gmail.com>
2019-05-19 09:25:05 +0700

    - add timezone in sequelize config

--- config/config.js
+++ config/config.js
@@ -1,10 +1,11 @@
 require('dotenv').config()
 module.exports = {
   [process.env.NODE_ENV]: {
-    "username": process.env.DB_USERNAME,
-    "password": process.env.DB_PASSWORD,
-    "database": process.env.DB_DATABASE,
-    "host": process.env.DB_HOST,
-    "dialect": "mysql"
-  }
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_DATABASE,
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+    timezone: '+07:00',
+  },
 }
